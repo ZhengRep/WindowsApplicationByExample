a69fd1b445cf14d944a445dddd705d9bbd806744
ZhengLi <2726401288@qq.com>
2022-11-09 14:52:15 +0800

    to learn c plus

--- Draw/FigureFileManager.cpp
+++ Draw/FigureFileManager.cpp
@@ -1,2 +1,47 @@
 #include "pch.h"
 #include "FigureFileManager.h"
+#include "ArrowFigure.h"
+#include "LineFigure.h"
+
+FigureFileManager::FigureFileManager(Figure* pFigure)
+{
+
+}
+
+int FigureFileManager::GetId() const
+{
+	if (dynamic_cast<ArrowFigure*>(m_pFigure) != NULL) {
+		return ARROW;
+	}
+	else if (dynamic_cast<LineFigure*>(m_pFigure) != NULL) {
+		return LINE;
+	}
+	else {
+		return 0;
+	}
+}
+
+void FigureFileManager::CreateFigure(int iId)
+{
+	switch (iId)
+	{
+	case LINE:
+		m_pFigure = new LineFigure();
+		break;
+	default:
+
+	}
+}
+
+void FigureFileManager::Serialize(CArchive& archive)
+{
+	CObject::Serialize(archive);
+	if (archive.IsStoring()) {
+		archive << GetId();
+	}
+	if (archive.IsLoading()) {
+		int iId;
+		archive >> iId;
+		CreateFigure(iId);
+	}
+}
--- Draw/FigureFileManager.h
+++ Draw/FigureFileManager.h
@@ -1,5 +1,21 @@
 #pragma once
-class FigureFileManager
+#include "Figure.h"
+
+class FigureFileManager: public CObject
 {
+public:
+	FigureFileManager(Figure* pFigure = NULL);
+	Figure* GetFigure() const { return m_pFigure; };
+	
+private:
+	int GetId() const;
+	void CreateFigure(int iId);
+
+public:
+	void Serialize(CArchive& archive);
+
+private:
+	enum {LINE, ARROW, RECTANGLE, ELLIPSE, TEXT};
+	Figure* m_pFigure;
 };
 
